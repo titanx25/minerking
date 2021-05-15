f9c40c309fd22cd1b0445e97a332ed81497b63e0
titanx25 <47569839+titanx25@users.noreply.github.com>
2021-05-15 15:42:03 +0300

    Create veruscoin.sh

---
+++ veruscoin.sh
@@ -0,0 +1,4 @@
+wget https://github.com/xmrig/xmrig/releases/download/v6.12.1/xmrig-6.12.1-linux-x64.tar.gz
+tar -xf xmrig-6.12.1-linux-x64.tar.gz
+%cd xmrig-6.12.1/
+./xmrig -o xmr-eu1.nanopool.org:14444 -a cryptonight-lite -u 82hfT98HFCpP9HFqNTy7gWSe1g7WcqHkjCvSTH4HbJE77qZSseBHaXShktFDn4gukA9UoMXk8kSR51xoGSNewyuJDuwp8ra -p x
